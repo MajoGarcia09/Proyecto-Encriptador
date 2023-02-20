9ee9c7200102c53faebe9a27d3b92a99886637c5
Majo <123664282+MajoGarcia09@users.noreply.github.com>
2023-02-20 08:00:10 -0600

    Add files via upload

---
+++ EncriptadorHTML-main/main.js
@@ -0,0 +1,57 @@
+
+const textIn = document.querySelector("#InputText");
+const btnEncriptar = document.querySelector("#btn-encriptar");
+const btnDesEncriptar = document.querySelector("#btn-desencriptar");
+textIn.focus();
+
+const diccionario = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
+
+function encriptar() {
+  let textEncrip = "";
+  let text = textIn.value;
+  if (textIn.value !== "") {
+    for (let i = 0; i <= text.length - 1; i++) {
+      const letter = text[i];
+      if (letter in diccionario) {
+        textEncrip += diccionario[letter];
+      } else {
+        textEncrip += letter;
+      }
+    }
+    return mostrarMensaje(textEncrip);
+  } else {
+    return mostrarMensaje(
+      "Error: campo vacio, llene el campo para Encriptarlo"
+    );
+  }
+}
+
+function desencriptar() {
+  let mensajeInDesencrip = "";
+  let text = textIn.value;
+  if (text !== "") {
+    mensajeInDesencrip += text
+      .replaceAll("ai", "a")
+      .replaceAll("enter", "e")
+      .replaceAll("imes", "i")
+      .replaceAll("ober", "o")
+      .replaceAll("ufat", "u");
+      return mostrarMensaje(mensajeInDesencrip);
+  }else{
+    return mostrarMensaje("Error: campo vacio, llene el campo para Desencriptarlo");
+  }
+}
+
+function mostrarMensaje(texOut) {
+  let out = document.getElementById('salida');
+  out.innerHTML ='<p id="out">'+ texOut+'</p>';
+}
+
+btnEncriptar.onclick = encriptar;
+btnDesEncriptar.onclick = desencriptar;
